90c6d829e45a56bde3ea8d10b6348cf2bd3f30d5
Kyle Schafer <kschay@protonmail.com>
2019-07-08 12:14:26 -0500

    Closes #16 - Created tests for Category, RecipeServiceImpl, and IndexController

---
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -0,0 +1,71 @@
+package guru.springframework.controllers;
+
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+public class IndexControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    @Mock
+    Model model;
+
+    IndexController controller;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        controller = new IndexController(recipeService);
+    }
+
+    @Test
+    public void getIndexPage() {
+
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+
+        //make sure both recipes are not equal
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        //verify getRecipes returns recipes
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        //create an ArgumentCaptor for the Set class
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
+        String viewName = controller.getIndexPage(model);
+
+        //then
+        //verify getIndexPage returns "index"
+        assertEquals("index", viewName);
+
+        //verify getRecipes in recipeService is called only once
+        verify(recipeService, times(1)).getRecipes();
+
+        //verify adding one attribute named "recipes" and capture the argument in argumentCaptor
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+
+        //verify both recipes are in the captured set
+        Set<Recipe> setIndexController = argumentCaptor.getValue();
+        assertEquals(2, setIndexController.size());
+    }
+}
