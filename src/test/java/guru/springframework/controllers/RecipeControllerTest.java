5e86d69142530debf236b2e7ed44658c2f47f5c6
Kyle Schafer <kschay@protonmail.com>
2019-07-15 11:52:10 -0500

    For #57 - Added 404error page and exception handler in RecipeController

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -56,7 +56,8 @@ public class RecipeControllerTest {
         when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
 
         mockMvc.perform(get("/recipe/1/show"))
-                .andExpect(status().isNotFound());
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404error"));
     }
 
     @Test
